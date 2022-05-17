6120ea93507286569b490063f455b3d2fda6b850
Felecia Davis <fmdavis1@gmail.com>
2022-05-17 17:18:32 -0400

    module for  npm work today

---
+++ fsmodule/app.js
@@ -0,0 +1,11 @@
+//importing the fs module
+// const fs = require('fs')
+
+// let message = 'Hello World!!!'
+
+// console.log(fs)
+// fs.writeFile('./notes.txt', message,function(){
+//     console.log(`Done crating file`)
+// } )
+const chalk = require('chalk')
+console.log(chalk.blue('Hello') )
